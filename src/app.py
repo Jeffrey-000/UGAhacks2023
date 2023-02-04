f69653fbf96bc99409c65e6d5e330e06e9b22ab5
Jeffaaaaaah <jeffaaaaaah@gmail.com>
2023-02-04 14:15:49 -0500

    home page stuff

--- src/app.py
+++ src/app.py
@@ -1,20 +1,27 @@
 import os 
 from flask import Flask as onlyfans
 from flask import render_template, request  
+from dotenv import load_dotenv
+
+load_dotenv()
+KEY = os.getenv("GOOGLE")
+
 your_mom = __name__
 
 app = onlyfans(your_mom)
 
+googlemapsurl = "https://www.google.com/maps/embed/v1/place?key=" + KEY + "&q=Space+Needle,Seattle+WA"
+
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", url=googlemapsurl)
 
 @app.route('/', methods=['POST', 'GET'])
 def calc(start=None, end=None):
     if request.method == "POST":
         start = request.form['start']
         end = request.form['end']
-    return render_template("map.html", start=start, end=end)
+    return render_template("map.html", url=googlemapsurl,start=start, end=end)
 
 
 #opens a developmental server when play button is run
