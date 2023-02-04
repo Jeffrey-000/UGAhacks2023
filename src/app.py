b6ff8a5349f569ade80d686cf059b278f650efe1
Jeffaaaaaah <jeffaaaaaah@gmail.com>
2023-02-04 13:27:08 -0500

    add google map embed

--- src/app.py
+++ src/app.py
@@ -1,17 +1,26 @@
 import os 
 from flask import Flask as onlyfans
+from flask import render_template, request  
 your_mom = __name__
 
 app = onlyfans(your_mom)
 
 @app.route('/')
-def hello_world():
-    return "<p>Hello World</p>"
+def home():
+    return render_template("home.html")
+
+@app.route('/', methods=['POST', 'GET'])
+def calc(start=None, end=None):
+    if request.method == "POST":
+        start = request.form['start']
+        end = request.form['end']
+    return render_template("map.html", start=start, end=end)
 
 
 #opens a developmental server when play button is run
+#make sure all code goes above these lines
 def runApp():
     os.system("flask --app ./src/app.py run")
 
 if __name__ == "__main__":
-    runApp()
+    app.run(debug=True)
